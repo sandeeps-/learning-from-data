7792c4fdd3f1bedabcad12a3424c4a1f2343ace2
Sandeep Shenoy <sandeepjshenoy@gmail.com>
2015-10-21 08:37:36 -0700

    Add support for non linear target functions

--- LinearModelLearning.py
+++ LinearModelLearning.py
@@ -6,7 +6,7 @@ Created on Thu Oct 01 15:06:10 2015
 """
 import numpy as np
 import matplotlib.pyplot as plt
-#Linear modelling experiments
+#Linear regression experiments
 
 def slope2D(x1, y1, x2, y2):
     return (y1 - y2) / (x1 - x2)
@@ -22,6 +22,9 @@ def target_func(x, y, m, c):
         else:
             return 1
 
+def target_func_nonlinear(x):
+    return np.sign(x[:, 1]**2 + x[:, 2]**2 - 0.6)
+    
 def hx(w, x):
     global N
     h = np.sign(x * w)
@@ -41,6 +44,23 @@ def init_trial_data(N):
 #            plt.plot(xn[j, 1], xn[j, 2], '-bx')
 #        else:
 #            plt.plot(xn[j, 1], xn[j, 2], '-rx')
+    w = np.matrix(np.zeros([D + 1, 1]))
+    return [xn, yn, w]
+
+def init_trial_data_2(N):
+    xn = np.matrix(np.random.rand(N, 2) * 2 - 1)
+    xn = np.concatenate([np.matrix(np.ones([N, 1])), xn], axis=1)
+    yn = np.matrix(np.zeros([N, 1]))
+
+    for j in range(0, np.size(yn)):
+        yn[j] = target_func_nonlinear(xn[j])
+#        if yn[j] == 1:
+#            plt.plot(xn[j, 1], xn[j, 2], '-bx')
+#        else:
+#            plt.plot(xn[j, 1], xn[j, 2], '-rx')
+    #Add simulated noise to 10% of output samples
+    for k in range(0, N/10):
+        yn[10 * k + np.random.randint(0, 10)] = yn[10 * k + np.random.randint(0, 10)] * -1 
     w = np.matrix(np.zeros([D + 1, 1]))
     return [xn, yn, w]
     
@@ -67,8 +87,8 @@ def run_PLA(xn, yn, w, maxItr):
 
 def run_linear_regression(xn, yn):
     global D
-    X_pseudo_inv =  np.linalg.inv(np.transpose(xn) * xn) * np.transpose(xn)
-    #X_pseudo_inv = np.linalg.pinv(xn)
+    #X_pseudo_inv =  np.linalg.inv(np.transpose(xn) * xn) * np.transpose(xn)
+    X_pseudo_inv = np.linalg.pinv(xn)
     w = X_pseudo_inv * yn
     Ein = compute_Ein(compute_error_vec(xn, yn, w), np.size(yn))
     return [np.reshape(w, (1, D + 1)), Ein]
@@ -84,8 +104,8 @@ def compute_Ein(error_vec, size):
     return 1.0 * np.size(error_vec) / size  
     
 if __name__ == "__main__":
-    N = 100   # Number of sample points
-    D = 2
+    N = 1000   # Number of sample points
+    D = 5
     M = 1000   #training data size
     totalTests = 1000
     maxItr = 1000    #max interations for PLA algorithm
@@ -94,12 +114,19 @@ if __name__ == "__main__":
     Eout_list = np.zeros([1, totalTests])
     g = np.matrix(np.zeros([totalTests, D + 1]))
     for i in range(0, totalTests):
-        [xn, yn, w] = init_trial_data(N + M)        
-        [g[i], Ein_list[0][i]] = run_linear_regression(xn[0:N], yn[0:N])
-        Eout_list[0][i] = compute_out_of_sample_error(xn[N:N + M], yn[N: N + M], g[i])
-        itrList[0][i] = run_PLA(xn[0:N], yn[0:N], np.transpose(g[i]), maxItr)
-
+#        [xn, yn, w] = init_trial_data(N + M)        
+#        [g[i], Ein_list[0][i]] = run_linear_regression(xn[0:N], yn[0:N])
+#        Eout_list[0][i] = compute_out_of_sample_error(xn[N:N + M], yn[N: N + M], g[i])
+#        itrList[0][i] = run_PLA(xn[0:N], yn[0:N], np.transpose(g[i]), maxItr)
+         [xn, yn, w] = init_trial_data_2(N + M)
+         x1 = np.matrix(np.array(xn[:, 1]) * np.array(xn[:, 2]))
+         x2 = np.matrix(np.array(xn[:, 1]) ** 2)
+         x3 = np.matrix(np.array(xn[:, 2]) ** 2)
+         xn = np.concatenate([xn, x1, x2, x3], axis=1)
+         [g[i], Ein_list[0][i]] = run_linear_regression(xn[0:N], yn[0:N])
+         Eout_list[0][i] = compute_out_of_sample_error(xn[N:N + M], yn[N: N + M], g[i])
     print("Average in sample error Ein %f" %np.average(Ein_list))
     print("Average out sample error Eout %f" %np.average(Eout_list))
+    print("Average weight %s" %np.average(g, axis=0))
 
     print("Average number of iterations %d" %np.average(itrList[np.where(itrList != maxItr - 1)]))
